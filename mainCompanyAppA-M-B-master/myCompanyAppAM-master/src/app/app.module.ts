6c2ba25a3ee1662aff6c23c2234d45a73406bf34
tej0897 <tej0897@gmail.com>
2022-11-16 01:17:15 +0530

    Login and Register working without JWT Auth

--- mainCompanyAppA-M-B-master/myCompanyAppAM-master/src/app/app.module.ts
+++ mainCompanyAppA-M-B-master/myCompanyAppAM-master/src/app/app.module.ts
@@ -4,27 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatGridListModule} from '@angular/material/grid-list';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
-import {MatDialogModule} from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
 import { DialogComponent } from './dialog/dialog.component';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatSortModule } from '@angular/material/sort';
 import { Dialog2Component } from './dialog2/dialog2.component';
-import {MatCardModule} from '@angular/material/card';
+import { MatCardModule } from '@angular/material/card';
 import { CompanyDashboardComponent } from './company-dashboard/company-dashboard.component';
-
-
-
-
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
@@ -32,9 +31,11 @@ import { CompanyDashboardComponent } from './company-dashboard/company-dashboard
     DialogComponent,
     Dialog2Component,
     CompanyDashboardComponent,
-    
+    LoginComponent,
+    SignupComponent,
   ],
   imports: [
+    MatGridListModule,
     FormsModule,
     MatCardModule,
     MatFormFieldModule,
@@ -52,9 +53,8 @@ import { CompanyDashboardComponent } from './company-dashboard/company-dashboard
     MatInputModule,
     ReactiveFormsModule,
     HttpClientModule,
-    
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
